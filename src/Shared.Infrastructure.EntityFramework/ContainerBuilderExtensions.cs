5204eb311119377014894b276d156eb06aee74bb
scheshan <5373827@qq.com>
2016-11-19 07:22:57 +0800

    add EntityFramework support

--- src/Shared.Infrastructure.EntityFramework/ContainerBuilderExtensions.cs
+++ src/Shared.Infrastructure.EntityFramework/ContainerBuilderExtensions.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Shared.Infrastructure.EntityFramework
+namespace Shared.Infrastructure
 {
     public static class ContainerBuilderExtensions
     {
