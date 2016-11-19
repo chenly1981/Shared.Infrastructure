23e315636fb0944d80da3f984590d41ab69a470b
scheshan <5373827@qq.com>
2016-11-19 10:11:35 +0800

    add logic

--- src/Shared.Infrastructure/UnitOfWork/IUnitOfWork.cs
+++ src/Shared.Infrastructure/UnitOfWork/IUnitOfWork.cs
@@ -2,22 +2,29 @@
 
 namespace Shared.Infrastructure.UnitOfWork
 {
+    /// <summary>
+    /// The UnitOfWork instance
+    /// </summary>
     public interface IUnitOfWork: IDisposable
     {
         /// <summary>
-        /// 传入仓储接口类型，获得该类型的仓储接口
+        /// Create a custom repository
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         T CreateRepository<T>() where T : IRepository;
 
         /// <summary>
-        /// 传入实体类型，获得提供了基础CRUD方法的默认仓储接口
+        /// Create a default repository
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         IRepository<T> CreateDefaultRepository<T>() where T : class, IEntity;
 
+        /// <summary>
+        /// Create a transaction
+        /// </summary>
+        /// <returns></returns>
         ITransaction BeginTransaction();
     }
 }
